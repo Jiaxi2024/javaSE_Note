d6fa1bcfed0f1af82f013c402bb2b1c0040d0535
Jiaxi2024 <160396090+Jiaxi2024@users.noreply.github.com>
2025-12-12 12:10:26 -0600

    Create README with Java SE learning outline
    Added a learning notes outline for Java SE.

--- fanxing/ListUtilTest.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class ListUtilTest {
-    public static void main(String[] args) {
-        ArrayList<String> list1 = new ArrayList<>();
-        ListUtil.addAll(list1,"123","aaa");
-        System.out.println(list1);
-    }
-}
